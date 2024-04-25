bc6286d0d29006a6ff56aaa5998c41d6e8ca0c07
Oliver <oliver@q-lado.de>
2024-04-25 10:49:30 +0200

    Changed whole thing up. Now i can save files on os. But it always has same name, to analyse it later

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
-from flask import Flask, render_template, request
-import cv2
-import numpy as np
+
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
 from PIL import Image, ExifTags
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    if 'picture' not in request.files:
-        return "No picture uploaded", 400
+@app.route('/', methods=['GET',"POST"])
+@app.route('/home', methods=['GET',"POST"])
 
-    picture = request.files['picture']
-    nparr = np.frombuffer(picture.read(), np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename('analyseFile.jpg'))) # Then save the file
+        return analyse()
+    return render_template('index.html', form=form)
 
+def analyse():
     # Perform your analysis on the image here
     # For example, you can use OpenCV or PIL to analyze the image
     # extract EXIF data
-    img = Image.open('JPCY7244.JPG')
+    
+    img = Image.open('static/files/analyseFile.jpg')
     exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
     
     exportString = ""
@@ -30,12 +40,16 @@ def analyze():
     for i in exif.keys():
             exportString += f"{i}: {exif[i]}\n "
 
+    return exportString
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
+""""
     # For demonstration, let's just return the dimensions of the image
     height, width, _ = image.shape
     analysis_result = f"Image dimensions: {width}x{height}"
     result = exportString + ", " + analysis_result
-
-    return result
-
-if __name__ == '__main__':
-    app.run(debug=True)
+"""
