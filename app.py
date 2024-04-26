8f0bac5df9eb85bd5ee6b3cce844ee31a4f9ce81
Oliver <oliver@q-lado.de>
2024-04-26 11:21:17 +0200

    Refactor app.py to save uploaded files with unique names and display analyzed image in index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -24,7 +24,8 @@ def home():
     if form.validate_on_submit():
         file = form.file.data # First grab the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename('analyseFile.jpg'))) # Then save the file
-        return analyse()
+        exif_data, analyzed_image = analyse()
+        return render_template('index.html', form=form, exif_data=exif_data, image_path=analyzed_image)
     return render_template('index.html', form=form)
 
 def analyse():
@@ -39,17 +40,22 @@ def analyse():
     
     for i in exif.keys():
             exportString += f"{i}: {exif[i]}\n "
+            
+    
+    img_black_white = img.convert('1')
+    
+    
+    img_black_white.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/files/analyseFile.jpg'))
 
-    return exportString
+    return exportString, 'static/files/analyseFile.jpg'
+
+#Method to get the image, is used in the index.html file
+@app.route('/get-image/<path:image_path>')
+def get_image(image_path):
+    return send_file(image_path)
     
 
 if __name__ == '__main__':
     app.run(debug=True)
     
     
-""""
-    # For demonstration, let's just return the dimensions of the image
-    height, width, _ = image.shape
-    analysis_result = f"Image dimensions: {width}x{height}"
-    result = exportString + ", " + analysis_result
-"""
