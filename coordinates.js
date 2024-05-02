b876fb53785996c0019e5f7c0c1ea4771b44e7d3
ialqas <oliver@q-lado.de>
2024-05-02 16:14:56 +0200

    coordinates file

---
+++ coordinates.js
@@ -0,0 +1,22 @@
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+  }
+  
+  function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+    var earthRadiusKm = 6371;
+  
+    var dLat = degreesToRadians(lat2-lat1);
+    var dLon = degreesToRadians(lon2-lon1);
+  
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+  
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    resultM = (earthRadiusKm * c)*1000;
+    return resultM;
+  }
+
+s = distanceInKmBetweenEarthCoordinates(8.6606764, 49.8800241, 8.6605142, 49.8800075); // 559.227457
+console.log(s);
