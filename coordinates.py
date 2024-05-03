5e3c9a08e3f1cd9bea70ee2729e9d72237149ed4
ialqas <oliver@q-lado.de>
2024-05-03 16:14:20 +0200

    Add import statement for 'analyse' function in coordinates.py

--- coordinates.py
+++ coordinates.py
@@ -1,4 +1,5 @@
 import geopy.distance
+from app import analyse
 
 coords_1 = (8.660541,
               49.8798987)
@@ -7,3 +8,6 @@ coords_2 = (8.6607032,
 
 print(geopy.distance.geodesic(coords_1, coords_2).meters)
 
+exif_data, analyzed_image = analyse()
+
+print(exif_data)
