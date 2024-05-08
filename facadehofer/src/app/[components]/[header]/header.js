d3809cbfe35bcb7b0ba9062b985c9516a08cb13f
Moritz <mail@moritzhorn.de>
2024-05-08 20:35:07 +0200

    basic js and tailwind stuff

---
+++ facadehofer/src/app/[components]/[header]/header.js
@@ -0,0 +1,7 @@
+
+
+const Header = () => (
+    <div class="w-96 bg-slate-600"></div>
+);
+
+export default Header;
--- facadehofer/src/app/page.tsx
+++ facadehofer/src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Wilkommen, bitte Adresse eingeben:</h1>
-      <input type="text" /> 
+      <h1>Willkommen, bitte Adresse eingeben:</h1>
+      <input type="text" />
       <button>Suchen</button>
       
     </div>
