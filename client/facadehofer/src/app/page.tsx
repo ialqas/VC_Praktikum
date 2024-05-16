6811ae23c31500070bee0046f103770e6ae1b7c9
timostra <timo.strassburger@gmail.com>
2024-05-16 13:22:59 +0200

    Möchtegern Header

--- client/facadehofer/src/app/page.tsx
+++ client/facadehofer/src/app/page.tsx
@@ -3,7 +3,7 @@ import MainWindow from "./[components]/[MainWindow]/MainWindow";
 
 export default function Home() {
   return (
-    <div className="font-coolvetica text-xl">
+    <div className="font-coolvetica text-xl bg-slate-100">
       <Header />
 
       <MainWindow />
